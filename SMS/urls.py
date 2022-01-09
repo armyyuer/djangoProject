3510ed1d52cb8f02f135c75526344c1d28421fde
armyyuer <11862810@qq.com>
2022-01-09 08:41:00 +0800

    20220109

--- SMS/urls.py
+++ SMS/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from SMS.views import *
 
 urlpatterns = [
+    path('', index),
     path('index/', index),
     path('tologin/', tologin_views),
     path('login/', login_views),

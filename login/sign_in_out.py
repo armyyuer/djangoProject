aecd35aa066f42928f23cd052c53d07dfc909d6c
armyyuer <11862810@qq.com>
2022-03-30 00:47:56 +0800

    home

--- login/sign_in_out.py
+++ login/sign_in_out.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.models import User
+from common.models import Company
 
 
 # 登录处理
@@ -22,6 +24,14 @@ def signin(request):
 
     # 如果能找到用户，并且密码正确
     if user is not None:
+
+        du = User.objects.get(username=userName)
+        request.session['username'] = userName
+        request.session['userid'] = du.id
+        request.session['useremail'] = du.email
+        dc = Company.objects.get(userId=du.id)
+        request.session['companyId'] = dc.companyId
+        request.session['companyName'] = dc.companyName
         if user.is_active:
             if user.is_superuser:
                 login(request, user)
@@ -29,14 +39,14 @@ def signin(request):
                 request.session['usertype'] = '1'
 
                 # return JsonResponse({'ret': 0})
-                response.write("<script>alert('管理员登陆成功！');window.location.href='/home';</script>")
+                response.write("<script>alert('管理员"+str(request.session['username'])+"登陆成功！');window.location.href='/';</script>")
                 return response
             else:
                 login(request, user)
                 # 在session中存入用户类型
                 request.session['usertype'] = '0'
                 # return JsonResponse({'ret': 1, 'msg': '请使用管理员账户登录'})
-                response.write("<script>alert('登陆成功！');window.location.href='/home';</script>")
+                response.write("<script>alert('["+str(request.session['companyName'])+"]登陆成功！');window.location.href='/';</script>")
                 return response
         else:
             # return JsonResponse({'ret': 0, 'msg': '用户已经被禁用'})
@@ -46,7 +56,9 @@ def signin(request):
     # 否则就是用户名、密码有误
     else:
         # return JsonResponse({'ret': 1, 'msg': '用户名或者密码错误'})
-        return HttpResponse('用户名或者密码错误！   [ <a href="javascript:history.go(-1)">返回</a> ]')
+        # return HttpResponse('用户名或者密码错误！   [ <a href="javascript:history.go(-1)">返回</a> ]')
+        response.write("<script>alert('用户名或者密码错误！');window.location.href='/login';</script>")
+        return response
 
 
 # 登出处理

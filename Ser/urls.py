1d3e6c2bff8343e495b4ec6166a9251f1ef30b2f
armyyuer <11862810@qq.com>
2022-09-12 16:15:02 +0800

    03

--- Ser/urls.py
+++ Ser/urls.py
@@ -1,8 +1,10 @@
 # 菜鸟程序员：阿米
 from django.urls import path
-from Ser.views import index_views
+from Ser.views import index_views, addflow, addflowsave
 
 urlpatterns = [
     path('', index_views, name='index'),
     path('index/', index_views, name='index'),
+    path('addflow/', addflow, name='addflow'),
+    path('addflowsave/', addflowsave, name='addflowsave'),
 ]
--- Ser/views.py
+++ Ser/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from common.models import SerOrders
+
 
 def index_views(request):
     return render(request, 'ser/index.html')
+
+
+def addflow(request):
+    return render(request, 'ser/addflow.html')
+
+
+def addflowsave(request):
+    company = request.POST.get("company", '')
+    address = request.POST.get("address", '')
+    contact = request.POST.get("contact", '')
+    tel = request.POST.get("tel", '')
+    content = request.POST.get("content", '')
+    hopeTime = request.POST.get("hopeTime", '')
+    remarks = request.POST.get("remarks", '')
+    record = SerOrders.objects.create(typeName=typeName)
+    print("新增流程类型：" + record.typeName)
+    return render(request, 'ser/addflow.html')
